333de2a56058956e77a6bb3a515e7e4adf38a71d
nabin893 <nayeemkhondoker@gmail.com>
2024-11-23 10:59:41 +0600

    first commit

---
+++ dome.js
@@ -0,0 +1,94 @@
+
+
+// const titel = document.getElementsByTagName("h2")
+// console.log(titel[1].innerText);
+
+
+// const titel = document.getElementsByClassName("bondhu")
+// for(tai of titel){
+//     console.log(tai);
+    
+// }
+
+
+
+// const titel = document.getElementById("lorem")
+//  console.log(titel.innerText);
+
+
+
+// const titel = document.getElementById("first")
+// first.innerText ="Tome change.next"
+
+// first.style.backgroundColor="blue"
+// first.style.Color="white"
+// first.style.broderRadius="16px"
+// first.style.margin="50px"
+// first.style.padding="50px"
+// first.style.textAlign="center"
+
+
+
+
+
+// console.log(hanejabe);
+
+
+
+// fist.style.margin ="50px";
+// fist.style.width ="50%";
+
+
+
+
+
+//  const container = document.getElementById("container");
+// console.log(container.innerText);
+
+ //  console.log(container.childNodes[0].nextSibling.innerText);
+ 
+// container. setAttribute("class","riday");
+// container. classList.add ("shanto","desh");
+// container.classList. add ("nayeem");
+// container.classList. remove ("desh");
+
+
+
+// console.log(container.parentNode.parentNode);
+// console.log(container.childNodes[0].nextSibling.innertext);
+
+
+// const lorem = document. getElementsById(lorem);
+// lorem .setAttribute("class","ridoy");
+// const classGatKorbo =document.getElementsByClassName("riday");
+// lorem.removeAttribute("id");
+// console.log(classGatKorbo[0]);
+
+
+
+
+
+
+
+// ডিসপ্লেতে দেখানোর জন্য EventTarget, id innerText,ডায়নামিকভাবে তৈরি করা এবং একে অপরের মধ্যে রাখা
+
+// const select =document.getElementById(select)
+// console.log(select);
+
+
+// const h2 = document.createElement("h2");
+// h2.innerText="amar sonar bangla ami tmay valobashi"
+// select.appendChild(h2)
+
+
+// const ul = document.createElement("ul")
+// const li = document.createElement("li")
+// li.innerText = "lorem lorem loiueebn "
+
+// ul.appendChild(li)
+// select.appendChild(ul)
+
+// li.setAttribute("id","jatio");
+// const jatios = document.getElementById("jatio")
+// jatios.style.color="red"
+
